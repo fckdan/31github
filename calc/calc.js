93a113ec55fb6e87148964adf016ca2c091d7014
fckdan <150842033+fckdan@users.noreply.github.com>
2023-12-03 00:19:37 +0600

    Add files via upload

---
+++ calc/calc.js
@@ -0,0 +1,9 @@
+let a = ''; //first number
+let b = ''; //second number
+let sign = ''; 
+let finish = false;
+
+const digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', '*', '/'];
+
+const out = 
